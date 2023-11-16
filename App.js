cbf138f6829b37e576d18ae4c20be9130cc817c1
Vitor Almeida da Silva <51917475+vitoralme@users.noreply.github.com>
2023-11-16 13:42:29 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import Home from './Componentes/Home/index'
+
+
+function App() {
+  return <>
+  <StatusBar backgroundColor={"#000"} />
+    <Home />
+
+  </>
+}
+
+
+export default App;
